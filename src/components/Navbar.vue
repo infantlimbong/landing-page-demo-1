725fc2506ae532aa72114f1a2be56e13cdb8cad3
infantlimbong <infantlimbong12@gmail.com>
2024-09-13 21:47:07 +0700

    fix mobile navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,9 +7,9 @@
                 <div class="md:hidden">
                     <button @click="toggleNavbar" class="relative z-50 flex flex-col w-8 aspect-square items-center justify-center">
                         <!-- Hamburger Icon and X Icon with transition -->
-                        <span :class="{ 'transform rotate-45 transition-transform': isNavbarOpen }" class="block w-6 h-0.5 bg-black transition-all duration-300 rounded"></span>
-                        <span :class="{ 'opacity-0': isNavbarOpen }" class="block w-6 h-0.5 bg-black mt-1.5 transition-opacity duration-300 rounded"></span>
-                        <span :class="{ 'transform -rotate-45 -mt-2.5 transition-transform': isNavbarOpen }" class="block w-6 h-0.5 bg-black mt-1.5 transition-all duration-300 rounded"></span>
+                        <span :class="{ 'transform rotate-45 transition-transform': isNavbarOpen }" class="block w-6 h-0.5 bg-black rounded"></span>
+                        <span :class="{ 'opacity-0': isNavbarOpen }" class="block w-6 h-0.5 bg-black mt-1.5 rounded"></span>
+                        <span :class="{ 'transform -rotate-45 mt-[-10px] transition-transform': isNavbarOpen }" class="block w-6 h-0.5 bg-black mt-1.5 rounded"></span>
                     </button>
                 </div>
             </div>
@@ -33,22 +33,27 @@
             </ul>
         </div>
         <!-- Mobile Navbar -->
-        <div :class="{ 'translate-x-0': isNavbarOpen, '-translate-x-full': !isNavbarOpen }" class="fixed left-0 top-14 w-full h-full bg-white/10 backdrop-blur-lg trans shadow-lg transform transition-transform duration-300 md:hidden">
-            <ul class="space-y-4 px-6 bg-red-200/10 h-full">
-                <li>
-                    <a @click.prevent="scrollToSection('home')" :class="['text-white', linkClasses('home')]"> Home </a>
+        <div :class="{ 'translate-x-0': isNavbarOpen, '-translate-x-full': !isNavbarOpen }" class="fixed left-0 top-14 w-full h-full bg-black/20 backdrop-blur-lg trans shadow-lg transform transition-transform duration-300 md:hidden">
+            <ul class="px-6 h-full text-center flex flex-col justify-center pb-28">
+                <li class="w-full relative flex">
+                    <a @click.prevent="scrollToSection('home')" :class="['text-white py-4 w-full', linkClasses('home')]"> Home </a>
+                    <div :class="['absolute w-full h-[.5px] bottom-0 md:hidden', lineClasses('home')]"></div>
                 </li>
-                <li>
-                    <a @click.prevent="scrollToSection('gallery')" :class="['text-white', linkClasses('gallery')]"> Gallery </a>
+                <li class="w-full relative flex">
+                    <a @click.prevent="scrollToSection('gallery')" :class="['text-white py-4 w-full', linkClasses('gallery')]"> Gallery </a>
+                    <div :class="['absolute w-full h-[.5px] bottom-0 md:hidden', lineClasses('gallery')]"></div>
                 </li>
-                <li>
-                    <a @click.prevent="scrollToSection('ballroom')" :class="['text-white', linkClasses('ballroom')]"> Ballroom </a>
+                <li class="w-full relative flex">
+                    <a @click.prevent="scrollToSection('ballroom')" :class="['text-white py-4 w-full', linkClasses('ballroom')]"> Ballroom </a>
+                    <div :class="['absolute w-full h-[.5px] bottom-0 md:hidden', lineClasses('ballroom')]"></div>
                 </li>
-                <li>
-                    <a @click.prevent="scrollToSection('order')" :class="['text-white', linkClasses('order')]"> Online Order </a>
+                <li class="w-full relative flex">
+                    <a @click.prevent="scrollToSection('order')" :class="['text-white py-4 w-full', linkClasses('order')]"> Online Order </a>
+                    <div :class="['absolute w-full h-[.5px] bottom-0 md:hidden', lineClasses('order')]"></div>
                 </li>
-                <li>
-                    <a @click.prevent="scrollToSection('career')" :class="['text-white', linkClasses('career')]"> Career </a>
+                <li class="w-full relative flex">
+                    <a @click.prevent="scrollToSection('career')" :class="['text-white py-4 w-full', linkClasses('career')]"> Career </a>
+                    <div :class="['absolute w-full h-[.5px] bottom-0 md:hidden', lineClasses('career')]"></div>
                 </li>
             </ul>
         </div>
@@ -81,6 +86,11 @@ const handleScroll = () => {
 // Toggle mobile navbar
 const toggleNavbar = () => {
     isNavbarOpen.value = !isNavbarOpen.value
+    if (isNavbarOpen.value) {
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.body.style.overflow = ''
+    }
 }
 
 // Scroll to a section and close the mobile navbar
@@ -88,13 +98,18 @@ const scrollToSection = (section) => {
     const el = document.getElementById(section)
     if (el) {
         el.scrollIntoView({ behavior: 'smooth' })
-        isNavbarOpen.value = false // Close navbar on navigation
+        isNavbarOpen.value = false
+        document.body.style.overflow = ''
     }
 }
 
 // Get link class based on active section
 const linkClasses = (section) => {
-    return activeSection.value === section ? 'bg-blue-500 text-white rounded-md px-2 py-1' : 'text-gray-700 hover:text-blue-500 cursor-pointer'
+    return activeSection.value === section ? 'text-rose-700' : 'cursor-pointer'
+}
+
+const lineClasses = (section) => {
+    return activeSection.value === section ? 'bg-gradient-to-r from-transparent via-rose-700 to-transparent' : 'bg-gradient-to-r from-transparent via-white to-transparent'
 }
 
 onMounted(() => {
@@ -107,7 +122,6 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
-/* For smooth hamburger icon transition */
 button span {
     transition: all 0.3s ease;
 }
